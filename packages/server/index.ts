bdb30710aabcea5268d70bb7e432efd5f18f0ea2
Felipe Romero <felromero9@gmail.com>
2026-02-16 22:00:47 +0100

    Add dependencies, formating scripts.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,17 +1,20 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", async (req: Request, res: Response) => {
-    res.send(`hmmmm running on port: ${port} ${process.env.OPENAI_API_KEY}`);
+app.get('/', async (req: Request, res: Response) => {
+    res.send(`hmmmm running on port: ${port} holaa`);
+});
 
+app.get('/api/hello', async (req: Request, res: Response) => {
+    res.json({ message: 'Hello World' });
 });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-})
+});
